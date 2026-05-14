b06632cac7150a7d2a1d095c1d1881210e1062cf
Sagnik Das <sagnikdaslearns@gmail.com>
2026-05-14 09:47:26 +0700

    second commit fix the build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/VocabGameVN/',
   plugins: [react()],
+  base: '/VocabGameVN/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
